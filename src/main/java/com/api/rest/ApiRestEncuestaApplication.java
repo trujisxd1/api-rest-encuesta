1d30d7dba3dab9c65740d7f7e37d7d6ee6a41658
Gutavo Martinez <gutamvol262@gmail.com>
2023-07-25 18:07:50 -0600

    Initial commit

---
+++ src/main/java/com/api/rest/ApiRestEncuestaApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestEncuestaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestEncuestaApplication.class, args);
+    }
+
+}
